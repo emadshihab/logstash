7c2ec6c6190be1ceda5ee438dac6bc9aa4e661f2
Emad Shihab <emadshihab@gmail.com>
2020-02-29 08:30:35 -0500

    fix metrics

--- logstash-core/src/main/java/co/elastic/logstash/api/Metric.java
+++ logstash-core/src/main/java/co/elastic/logstash/api/Metric.java
@@ -12,3 +12,13 @@ public interface Metric {
      */
     NamespacedMetric namespace(String... key);
 }
+
+public interface Metric {
+    /**
+     * Creates a namespace under the current {@link Metric} and returns it.
+     *
+     * @param key namespace to traverse into
+     * @return the {@code key} namespace under the current Metric
+     */
+    NamespacedMetric namespace(String... key);
+}
